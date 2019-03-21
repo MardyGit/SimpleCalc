22074df8529a372739f81cca5679dd6c1465b02c
Peter Kibble <peter@q4technologies.com>
2019-03-21 13:08:43 +0000

    Added CalcButton

---
+++ SimpleCalc/SimpleCalc/SimpleCalc/Controls/CalcButton.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SimpleCalc.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalcButton : ContentView
+    {
+        public CalcButton()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        public static readonly new BindableProperty BackgroundColorProperty = BindableProperty.Create(nameof(BackgroundColor), typeof(Color), typeof(CalcButton), null);
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(CalcButton), null);
+        public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(nameof(FontSize), typeof(string), typeof(CalcButton), 18);
+        public static readonly BindableProperty CommandProperty = BindableProperty.Create(nameof(Command), typeof(Command), typeof(CalcButton), null);
+        public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(CalcButton), null);
+
+        public new Color BackgroundColor
+        {
+            get => (Color)GetValue(BackgroundColorProperty);
+            set => SetValue(BackgroundColorProperty, value);
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        [TypeConverter(typeof(FontSizeConverter))]
+        public double FontSize
+        {
+            get => (double)GetValue(FontSizeProperty);
+            set => SetValue(FontSizeProperty, value);
+        }
+
+        public Command Command
+        {
+            get => (Command)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public object CommandParameter
+        {
+            get => GetValue(CommandParameterProperty);
+            set => SetValue(CommandParameterProperty, value);
+        }
+    }
+}
